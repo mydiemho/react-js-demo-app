87ead36cfec6cf83b10266879aace91807a27634
My Ho <mydiemho@gmail.com>
2020-04-09 17:56:12 -0700

    setup cypress
    1. add sample specs

---
+++ src/cypress/integration/app_spec.js
@@ -0,0 +1,5 @@
+describe("App Test", () => {
+  it("Visits the app", () => {
+    cy.visit("/");
+  });
+});
---
+++ src/cypress/integration/sample_spec.js
@@ -0,0 +1,40 @@
+describe("My First Tests", () => {
+  it("Visits the Kitchen Sink", () => {
+    cy.visit("https://example.cypress.io");
+  });
+
+  it('finds the content "type"', () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type");
+  });
+
+  it('clicks the link "type"', () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+  });
+
+  it('clicking "type" navigates to a new url', () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+  });
+
+  it("Gets, types and asserts", () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get(".action-email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com");
+  });
+});
